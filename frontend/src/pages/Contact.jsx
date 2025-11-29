e4c527ed727b27bec00e43a69781f69ec679ecfc
melan-Akash <user.melan.akash@icloud.com>
2025-11-29 20:01:03 +0530

    Backend Start

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import Newsletter from '../components/Newsletter'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+
+          <p>
+            54894 Willms Station <br/>
+            Suite 350, Washington, USA
+          </p>
+
+          <p className='text-gray-700'>
+            Email : support@yourstore.com
+          </p>
+
+          <p className='text-gray-700'>
+            Phone : +1 800 123 4567
+          </p>
+
+          <p className='text-gray-700'>
+            Working Hours : Mon - Fri / 9:00 AM – 6:00 PM
+          </p>
+        </div>
+      </div>
+      <Newsletter />
     </div>
   )
 }
